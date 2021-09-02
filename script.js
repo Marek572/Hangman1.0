27117b435a2c347eb2d5d37d839800e83bd82c20
Marek Porębski <marek.bp@poczta.fm>
2021-09-02 11:59:26 +0200

    categories.wip

--- script.js
+++ script.js
@@ -1,9 +1,144 @@
-let newPhrase = "Zegarek"; //hasło
+let newPhrase = "test"; //hasło
 newPhrase = newPhrase.toUpperCase(); //kapitaliki
 let phrase = document.getElementById("phrase"); //element z hasłem
 let dashedPhrase = "";
 let phraseLength = newPhrase.length; //długość hasła
 let missAmount = 0;
+
+
+// //tablica kategorii
+// let categories = new Array(6);
+
+// categories[0] = "Gry"
+// categories[1] = "Marki Samochodów"
+// categories[2] = "Rośliny"
+// categories[3] = "Zwierzęta"
+// categories[4] = "Filmy"
+// categories[5] = "Wszystko"
+
+// //tablica gier
+// let games = new Array(20);
+
+// games[0] = "League of Legends"
+// games[1] = "World of Warcraft"
+// games[2] = "Wiedźmin"
+// games[3] = "Red Dead Redemption"
+// games[4] = "Ori"
+// games[5] = "Cuphead"
+// games[6] = "Dead Cells"
+// games[7] = "God of War"
+// games[8] = "Hollow Knight"
+// games[9] = "Ghost of tsushima"
+// games[10] = "Ghostrunner"
+// games[11] = "Speedrunners"
+// games[12] = "Hades"
+// games[13] = "The Last of Us"
+// games[14] = "Half-Life"
+// games[15] = "Dark Souls"
+// games[16] = "Doom"
+// games[17] = "Animal Crossing"
+// games[18] = "Assassins Creed"
+// games[19] = "Cyberpunk"
+
+// // tablica samochodów
+// let cars = new Array(20);
+
+// cars[0] = "Audi"
+// cars[1] = "BMW"
+// cars[2] = "Citroen"
+// cars[3] = "Dacia"
+// cars[4] = "Fiat"
+// cars[5] = "Ford"
+// cars[6] = "Honda"
+// cars[7] = "Hyudai"
+// cars[8] = "Volvo"
+// cars[9] = "Kia"
+// cars[10] = "Mercedes"
+// cars[11] = "Nissan"
+// cars[12] = "Opel"
+// cars[13] = "Peugeot"
+// cars[14] = "Renault"
+// cars[15] = "Seat"
+// cars[16] = "Skoda"
+// cars[17] = "Toyota"
+// cars[18] = "Volkswagen"
+// cars[19] = "Lexus"
+
+// // tablica roślin
+// let flora = new Array(20);
+
+// flora[0] = "Tulipan"
+// flora[1] = "Wanilia"
+// flora[2] = "Róża"
+// flora[3] = "Marchewka"
+// flora[4] = "Seler"
+// flora[5] = "Lawenda"
+// flora[6] = "Irys"
+// flora[7] = "Kaktus"
+// flora[8] = "Stokrotka"
+// flora[9] = "Koniczyna"
+// flora[10] = "Jodła"
+// flora[11] = "Świerk"
+// flora[12] = "Aloes"
+// flora[13] = "Storczyk"
+// flora[14] = "Hortensja"
+// flora[15] = "Bambus"
+// flora[16] = "Chaber"
+// flora[17] = "Babka zwyczjna"
+// flora[18] = "Barszcz zwyczajny"
+// flora[19] = "Dziurawiec zwyczajny"
+
+// // tablica zwierząt
+// let animals = new Array(20);
+
+// animals[0] = "Ślimak"
+// animals[1] = "Gołąb"
+// animals[2] = "Mewa"
+// animals[3] = "Lew"
+// animals[4] = "Tygrys"
+// animals[5] = "Wydra"
+// animals[6] = "Zając"
+// animals[7] = "Owca"
+// animals[8] = "Niedźwiedź brunatny"
+// animals[9] = "Panda"
+// animals[10] = "Wilk"
+// animals[11] = "Lis"
+// animals[12] = "Królik"
+// animals[13] = "Delfin"
+// animals[14] = "Wieloryb"
+// animals[15] = "Foka"
+// animals[16] = "Pantera"
+// animals[17] = "Żaba Wodna"
+// animals[18] = "Tarantula"
+// animals[19] = "Zaskroniec"
+
+// // tablica filów
+// let films = new Array(20);
+
+// films[0] = "Skazani na Shawshank"
+// films[1] = "Casablanca"
+// films[2] = "Titanic"
+// films[3] = "Forest Gump"
+// films[4] = "Władca Pierścieni"
+// films[5] = "Pulp Fiction"
+// films[6] = "Bękarty wojny"
+// films[7] = "Mad Max"
+// films[8] = "Nietykalni"
+// films[9] = "Zielona mila"
+// films[10] = "Ojciec chrzestny"
+// films[11] = "Dwunastu gniewnych"
+// films[12] = "Lot nad kukułczym gniazdem"
+// films[13] = "Joker"
+// films[14] = "Incepcja"
+// films[15] = "Django"
+// films[16] = "Król Lew"
+// films[17] = "Leon zawodowiec"
+// films[18] = "Gladiator"
+// films[19] = "Gwiezdne wojny"
+
+// // tablica z wszystkim
+// let combinedArrays = [games, cars, flora, animals, films]
+
 
 
 //tablica liter
@@ -45,6 +180,24 @@ letters[32] = "Z"
 letters[33] = "Ź"
 letters[34] = "Ż"
 
+//stworzenie divów z kategoriami
+// function createCategories(){
+//     let divContent ="";
+
+//     for(i=0;i<=5; i++){
+//         let element = "category" + i;
+//         divContent = divContent + '<div id="'+element+'" class="category" onclick="choseCategory('+i+')">'+categories[i]+'</div>';
+//     }
+
+//     document.getElementById("categories").innerHTML = divContent;
+
+//     createLetters();
+// }
+
+//wybieranie kategorii
+// function choseCategory(){
+
+// }
 
 //zamiana liter na kreski
 function loadPhrase(){
@@ -56,8 +209,9 @@ for(i=0; i<phraseLength; i++){
     else dashedPhrase = dashedPhrase + "-";
 }
 
+//stworzenie divów z literami
 function createLetters(){
-    var divContent ="";
+    let divContent ="";
 
     for(i=0;i<=34; i++){
         let element = "letter" + i;
@@ -113,7 +267,7 @@ function check(num){
     //wygrana
     if(newPhrase == dashedPhrase){
         document.getElementById("letters").innerHTML = '<button id="reset" onclick="location.reload()">Jeszcze raz</button>'
-        alert("Brawo! Wygrałeś!\nHasło to: "+newPhrase);
+        alert("Wygrałeś!\nHasło to: "+newPhrase);
     }
 
     //przegrana
