a57e1cbf3886c6fd59fa505d839785b933f879d8
Marek Porębski <marek.bp@poczta.fm>
2021-09-02 16:01:04 +0200

    Mirek :((

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let newPhrase = "test"; //hasło
+let newPhrase = "ABC"; //hasło
 newPhrase = newPhrase.toUpperCase(); //kapitaliki
 let phrase = document.getElementById("phrase"); //element z hasłem
 let dashedPhrase = "";
@@ -6,198 +6,69 @@ let phraseLength = newPhrase.length; //długość hasła
 let missAmount = 0;
 
 
-// //tablica kategorii
-// let categories = new Array(6);
+//tablica kategorii
+let categories = ["Gry","Marki Samochodów","Rośliny","Zwierzęta","Filmy","Wszystko"];
 
-// categories[0] = "Gry"
-// categories[1] = "Marki Samochodów"
-// categories[2] = "Rośliny"
-// categories[3] = "Zwierzęta"
-// categories[4] = "Filmy"
-// categories[5] = "Wszystko"
+//tablica gier
+let games = ["League of Legends","World of Warcraft","Wiedźmin","Red Dead Redemption","Ori","Cuphead","Dead Cells","God of War","Hollow Knight","Ghost of tsushima","Ghostrunner","Speedrunners","The Last of Us","Half-Life","Dark Souls","Doom","Animal Crossing","Assassins Creed","Cyberpunk"];
 
-// //tablica gier
-// let games = new Array(20);
+// tablica samochodów
+let cars = ["Audi","BMW","Citroen","Dacia","Fiat","Ford","Honda","Hyudai","Volvo","Kia","Mercedes","Nissan","Opel","Peugeot","Renault","Seat","Skoda","Toyota","Volkswagen","Lexus"];
 
-// games[0] = "League of Legends"
-// games[1] = "World of Warcraft"
-// games[2] = "Wiedźmin"
-// games[3] = "Red Dead Redemption"
-// games[4] = "Ori"
-// games[5] = "Cuphead"
-// games[6] = "Dead Cells"
-// games[7] = "God of War"
-// games[8] = "Hollow Knight"
-// games[9] = "Ghost of tsushima"
-// games[10] = "Ghostrunner"
-// games[11] = "Speedrunners"
-// games[12] = "Hades"
-// games[13] = "The Last of Us"
-// games[14] = "Half-Life"
-// games[15] = "Dark Souls"
-// games[16] = "Doom"
-// games[17] = "Animal Crossing"
-// games[18] = "Assassins Creed"
-// games[19] = "Cyberpunk"
-
-// // tablica samochodów
-// let cars = new Array(20);
-
-// cars[0] = "Audi"
-// cars[1] = "BMW"
-// cars[2] = "Citroen"
-// cars[3] = "Dacia"
-// cars[4] = "Fiat"
-// cars[5] = "Ford"
-// cars[6] = "Honda"
-// cars[7] = "Hyudai"
-// cars[8] = "Volvo"
-// cars[9] = "Kia"
-// cars[10] = "Mercedes"
-// cars[11] = "Nissan"
-// cars[12] = "Opel"
-// cars[13] = "Peugeot"
-// cars[14] = "Renault"
-// cars[15] = "Seat"
-// cars[16] = "Skoda"
-// cars[17] = "Toyota"
-// cars[18] = "Volkswagen"
-// cars[19] = "Lexus"
-
-// // tablica roślin
-// let flora = new Array(20);
-
-// flora[0] = "Tulipan"
-// flora[1] = "Wanilia"
-// flora[2] = "Róża"
-// flora[3] = "Marchewka"
-// flora[4] = "Seler"
-// flora[5] = "Lawenda"
-// flora[6] = "Irys"
-// flora[7] = "Kaktus"
-// flora[8] = "Stokrotka"
-// flora[9] = "Koniczyna"
-// flora[10] = "Jodła"
-// flora[11] = "Świerk"
-// flora[12] = "Aloes"
-// flora[13] = "Storczyk"
-// flora[14] = "Hortensja"
-// flora[15] = "Bambus"
-// flora[16] = "Chaber"
-// flora[17] = "Babka zwyczjna"
-// flora[18] = "Barszcz zwyczajny"
-// flora[19] = "Dziurawiec zwyczajny"
+// tablica roślin
+let flora = ["Tulipan","Wanilia","Róża","Marchewka","Seler","Lawenda","Irys","Kaktus","Stokrotka","Koniczyna","Jodła","Świerk","Aloes","Storczyk","Hortensja","Bambus","Chaber","Babka zwyczjna","Barszcz zwyczajny","Dziurawiec zwyczajny"];
 
 // // tablica zwierząt
-// let animals = new Array(20);
+let animals = ["Ślimak","Gołąb","Mewa","Lew","Tygrys","Wydra","Zając","Owca","Niedźwiedź brunatny","Panda","Wilk","Lis","Królik","Delfin","Wieloryb","Foka","Pantera","Żaba Wodna","Tarantula","Zaskroniec"];
 
-// animals[0] = "Ślimak"
-// animals[1] = "Gołąb"
-// animals[2] = "Mewa"
-// animals[3] = "Lew"
-// animals[4] = "Tygrys"
-// animals[5] = "Wydra"
-// animals[6] = "Zając"
-// animals[7] = "Owca"
-// animals[8] = "Niedźwiedź brunatny"
-// animals[9] = "Panda"
-// animals[10] = "Wilk"
-// animals[11] = "Lis"
-// animals[12] = "Królik"
-// animals[13] = "Delfin"
-// animals[14] = "Wieloryb"
-// animals[15] = "Foka"
-// animals[16] = "Pantera"
-// animals[17] = "Żaba Wodna"
-// animals[18] = "Tarantula"
-// animals[19] = "Zaskroniec"
-
-// // tablica filów
-// let films = new Array(20);
-
-// films[0] = "Skazani na Shawshank"
-// films[1] = "Casablanca"
-// films[2] = "Titanic"
-// films[3] = "Forest Gump"
-// films[4] = "Władca Pierścieni"
-// films[5] = "Pulp Fiction"
-// films[6] = "Bękarty wojny"
-// films[7] = "Mad Max"
-// films[8] = "Nietykalni"
-// films[9] = "Zielona mila"
-// films[10] = "Ojciec chrzestny"
-// films[11] = "Dwunastu gniewnych"
-// films[12] = "Lot nad kukułczym gniazdem"
-// films[13] = "Joker"
-// films[14] = "Incepcja"
-// films[15] = "Django"
-// films[16] = "Król Lew"
-// films[17] = "Leon zawodowiec"
-// films[18] = "Gladiator"
-// films[19] = "Gwiezdne wojny"
+// tablica filów
+let films = ["Skazani na Shawshank","Casablanca","Titanic","Forest Gump","Władca Pierścieni","Pulp Fiction","Bękarty wojny","Mad Max","Nietykalni","Zielona mila","Ojciec chrzestny","Dwunastu gniewnych","Lot nad kukułczym gniazdem","Joker","Incepcja","Django","Król Lew","Leon zawodowiec","Gladiator","Gwiezdne wojny"];
 
 // // tablica z wszystkim
 // let combinedArrays = [games, cars, flora, animals, films]
 
-
-
 //tablica liter
-let letters = new Array(35);
+let letters = ["A","Ą","B","C","Ć","D","E","Ę","F","G","H","I","J","K","L","Ł","M","N","Ń","O","Ó","P","Q","R","S","Ś","T","U","V","W","X","Y","Z","Ź","Ż"];
 
-letters[0] = "A"
-letters[1] = "Ą"
-letters[2] = "B"
-letters[3] = "C"
-letters[4] = "Ć"
-letters[5] = "D"
-letters[6] = "E"
-letters[7] = "Ę"
-letters[8] = "F"
-letters[9] = "G"
-letters[10] = "H"
-letters[11] = "I"
-letters[12] = "J"
-letters[13] = "K"
-letters[14] = "L"
-letters[15] = "Ł"
-letters[16] = "M"
-letters[17] = "N"
-letters[18] = "Ń"
-letters[19] = "O"
-letters[20] = "Ó"
-letters[21] = "P"
-letters[22] = "Q"
-letters[23] = "R"
-letters[24] = "S"
-letters[25] = "Ś"
-letters[26] = "T"
-letters[27] = "U"
-letters[28] = "V"
-letters[29] = "W"
-letters[30] = "X"
-letters[31] = "Y"
-letters[32] = "Z"
-letters[33] = "Ź"
-letters[34] = "Ż"
 
 //stworzenie divów z kategoriami
-// function createCategories(){
-//     let divContent ="";
+function createCategories(){
+    let divContent ="";
 
-//     for(i=0;i<=5; i++){
-//         let element = "category" + i;
-//         divContent = divContent + '<div id="'+element+'" class="category" onclick="choseCategory('+i+')">'+categories[i]+'</div>';
-//     }
+    for(i=0;i<=5; i++){
+        let element = "category" + i;
+        divContent = divContent + '<div id="'+element+'" class="category" value="'+element+'" onclick="choseCategory(this.value)">'+categories[i]+'</div>';
+    }
 
-//     document.getElementById("categories").innerHTML = divContent;
+    document.getElementById("categories").innerHTML = divContent;
 
-//     createLetters();
-// }
+    createLetters();
+}
 
-//wybieranie kategorii
-// function choseCategory(){
-
-// }
+// wybieranie kategorii
+function choseCategory(category){
+    switch(category){
+        case "category0":
+            newPhrase = games[Math.floor(Math.random()*games.length)];
+            break
+        case "category1":
+            newPhrase = cars[Math.floor(Math.random()*cars.length)];
+            break
+        case "category2":
+            newPhrase = cars[Math.floor(Math.random()*cars.length)];
+            break
+        case "category3":
+            newPhrase = cars[Math.floor(Math.random()*cars.length)];
+            break
+        case "category4":
+            newPhrase = cars[Math.floor(Math.random()*cars.length)];
+            break
+        case "category5":
+            newPhrase = "test"
+            break
+        
+    }
+}
 
 //zamiana liter na kreski
 function loadPhrase(){
@@ -277,4 +148,4 @@ function check(num){
     }
 }
 
-window.onload = createLetters;
+window.onload = createCategories;
